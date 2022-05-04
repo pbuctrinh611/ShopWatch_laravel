274ee56a988b6a8f5c960f114431617e13de046f
nhocvukhoa <71363631+nhocvukhoa@users.noreply.github.com>
2022-05-04 20:11:03 +0700

    display products and blogs on the homepage

--- app/Http/Controllers/User/UserHomeController.php
+++ app/Http/Controllers/User/UserHomeController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\Brand;
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class UserHomeController extends Controller
 {
     public function index() {
-        return view('user.home.index');
+        $product_new = Product::all();
+        $product_sale = Product::all();
+        $product_best = Product::all();
+        $brands = Brand::all();
+        $blogs = Blog::all();
+        return view('user.home.index', compact('product_new', 'product_sale', 'product_best', 'brands', 'blogs'));
     }
 }
