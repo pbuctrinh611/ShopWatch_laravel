ad4daa6333f6b91073eb816e4b96a9978197be24
huyphanpro456 <73130091+huyphanpro456@users.noreply.github.com>
2022-05-10 01:02:43 +0700

    Update ColorController.php

--- app/Http/Controllers/Admin/ColorController.php
+++ app/Http/Controllers/Admin/ColorController.php
@@ -4,8 +4,13 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Color;
 
 class ColorController extends Controller
 {
-    //
+    public function all()
+    {
+        $data = Color::all();
+        return response()->json($data);
+    }
 }
