35ddd400a1c19f92af30489b53fcb28a9edb205c
nhocvukhoa <71363631+nhocvukhoa@users.noreply.github.com>
2022-06-28 08:58:11 +0700

    Handle the number of employees counting wrong and update the chart of the number of orders by month

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -12,7 +12,10 @@ class HomeController extends Controller
 {
     public function home(Request $request) {
         DB::statement("SET SQL_MODE=''");
-        $total_employee = User::whereNotIn('id', [1,5])->count();
+        $total_manage = User::where('id_role', 2)->count();
+        $total_saler = User::where('id_role', 3)->count();
+        $total_shipper = User::where('id_role', 4)->count();
+        $total_employee = $total_manage + $total_saler + $total_shipper;
         $total_customer = User::where('id_role', 5)->count();
         $total_order_pay = Order::count();
         $total_money = Order::sum('total_money');
@@ -30,21 +33,21 @@ class HomeController extends Controller
         }
         $chartData = $data;
 
-        $user = User::select(DB::raw("COUNT(*) as count"))
+        $order = Order::select(DB::raw("COUNT(*) as count"))
             ->whereYear('created_at',date('Y'))
-            ->where('id_role', 5)
             ->groupBy(DB::raw("Month(created_at)"))
             ->pluck('count');
-        $months = User::select(DB::raw("Month(created_at) as month"))
+        $months = Order::select(DB::raw("Month(created_at) as month"))
         ->whereYear('created_at',date('Y'))
         ->groupBy(DB::raw("Month(created_at)"))
         ->pluck("month");
         $data_month =  [0,0,0,0,0,0,0,0,0,0,0,0];
         foreach ($months as $index =>$month){
             --$month;
-            $data_month[$month] = $user[$index];
+            $data_month[$month] = $order[$index];
         }
+
         return view('admin.home', compact('total_order', 'chartData', 'total_employee', 'total_customer', 'total_order_pay', 'total_money', 'months', 
-        'data_month', 'user'));
+        'data_month'));
     }
 }
